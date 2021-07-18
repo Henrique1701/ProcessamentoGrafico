b335f5103e568f292cb13b03ae30350ac0fc5fb7
André Luiz <alps2@cin.ufpe.br>
2021-07-17 23:21:25 -0300

    bezier js finished. It works but needs debugging

--- Projeto 2/js/bezier.js
+++ Projeto 2/js/bezier.js
@@ -20,7 +20,7 @@ function deCasteljau(pontos, t){
     } else {
         // nova curva
         var pontosAux = [];
-        for (i = 0; i < grau; i++){
+        for (let i = 0; i < grau; i++){
             pontosAux.push(interpolation(pontos[i], pontos[i+1], t));
         }
         return deCasteljau(pontosAux,  t);
@@ -29,29 +29,30 @@ function deCasteljau(pontos, t){
 
 // elementos da aplicação
 // canvas
-var canvas = document.getElementsByName("canvas");
+var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
 // legenda
-var legendaText = document.getElementsByName("legenda");
+var legendaText = document.getElementById("legenda");
 
 // botões - curva
-var newCurvaBtn = document.getElementsByName("newCurveBtn");
-var delCurvaBtn = document.getElementsByName("delCurveBtn");
-var previousCurveBtn = document.getElementsByName("previousCurveBtn");
-var nextCurveBtn = document.getElementsByName("nextCurveBtn");
+var newCurvaBtn = document.getElementById("newCurveBtn");
+var delCurvaBtn = document.getElementById("delCurveBtn");
+var previousCurveBtn = document.getElementById("previousCurveBtn");
+var nextCurveBtn = document.getElementById("nextCurveBtn");
 
 // botões - pontos
-var addPontoBtn = document.getElementsByName("addPontoBtn");
-var movePontoBtn = document.getElementsByName("movePontoBtn");
-var previousPontoBtn = document.getElementsByName("previousPontoBtn");
-var nextPontoBtn = document.getElementsByName("nextPontoBtn");
+var addPontoBtn = document.getElementById("addPontoBtn");
+var delPontoBtn = document.getElementById("delPontoBtn");
+var movePontoBtn = document.getElementById("movePontoBtn");
+var previousPontoBtn = document.getElementById("previousPontoBtn");
+var nextPontoBtn = document.getElementById("nextPontoBtn");
 
 // input e checkboxes
-var numAvaliacoesInput = document.getElementsByName("avaliacoes");
-var poligonosCheckbox = document.getElementsByName("poligonosCheckbox");
-var pontosCheckbox = document.getElementsByName("pontosCheckbox");
-var curvasCheckbox = document.getElementsByName("curvasCheckbox");
+var numAvaliacoesInput = document.getElementById("avaliacoes");
+var poligonosCheckbox = document.getElementById("poligonosCheckbox");
+var pontosCheckbox = document.getElementById("pontosCheckbox");
+var curvasCheckbox = document.getElementById("curvasCheckbox");
 
 // constantes e variáveis
 const POINT_RADIUS = 4; // para desenhar o ponto
@@ -95,7 +96,7 @@ function drawLine(a, b){
 
 // desenha poligonos de controle
 function drawControlPolygon(pontos){
-    for (i = 0; i < pontos.length-1; i++){
+    for (let i = 0; i < pontos.length-1; i++){
         drawLine(pontos[i], pontos[i+1]);
     }
 }
@@ -106,7 +107,7 @@ function drawBezierCurve(curve){
         var bezierCurves = [];
         bezierCurves.push(curve[0]);
         // a iteração é até numAvaliações - 2, pois já estão inclusos os pontos inicial e final, que são dados pelo usuário.
-        for (i = 1; i <= numAvaliacoes-2; i++){
+        for (let i = 1; i <= numAvaliacoes-2; i++){
             bezierCurves.push(deCasteljau(curve, i / numAvaliacoes));
         }
         // esse ultimo push fora do laço é para que ele não fique dando aquela sobrinha, entre a ultima avaliação e o ultimo ponto.
@@ -117,9 +118,10 @@ function drawBezierCurve(curve){
 
 // redesenhar
 function reDraw(){
+    console.log("began redraw");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     if (showPoligonos){
-        for (i = 0; i < curves.length; i++){
+        for (let i = 0; i < curves.length; i++){
             if (i == selectedCurve){
                 ctx.strokeStyle="pink";
             } else {
@@ -129,8 +131,8 @@ function reDraw(){
         }
     }
     if (showPoints){
-        for (i = 0; i < curves.length; i++){
-            for (j = 0; j < curves[i].length; j++){
+        for (let i = 0; i < curves.length; i++){
+            for (let j = 0; j < curves[i].length; j++){
                 if ((j == selectedPoint[selectedCurve]) && (i == selectedCurve)){
                     ctx.strokeStyle="orange";
                 } else {
@@ -141,7 +143,7 @@ function reDraw(){
         }
     }
     if (showCurves && (numAvaliacoes > 1)){
-        for (i = 0; i < curves.length; i++){
+        for (let i = 0; i < curves.length; i++){
             if (i == selectedCurve){
                 ctx.strokeStyle="green";
             } else {
@@ -150,4 +152,162 @@ function reDraw(){
             drawBezierCurve(curves[i]);
         }
     }
+    console.log("finished redraw");
 }
+
+// event listeners
+
+// canvas: reDraw é a função principal que vai ser sempre chamada para refletir em tempo real as mudanças
+
+// clique no mouse
+canvas.addEventListener("mousedown", function(event){
+    clique = true;
+    // novo ponto a
+    var a = new Point(event.offsetX, event.offsetY);
+    if (stateCanvas == 1){
+        curves[selectedCurve].push(a);
+    } else if (stateCanvas == 2){
+        curves[selectedCurve].splice(selectedPoint[selectedCurve], 1, a);
+    }
+    reDraw();
+});
+
+// movimento do mouse
+canvas.addEventListener("mousemove", function(event){
+    if (clique){
+        if (stateCanvas == 2){
+            var a = new Point(event.offsetX, event.offsetY);
+            curves[selectedCurve].splice(selectedPoint[selectedCurve], 1, a);
+        }
+    }
+    reDraw();
+});
+
+// solta o mouse
+canvas.addEventListener("mouseup", function(event){
+    clique = false;
+    reDraw();
+    console.log("mouseup");
+});
+
+// botões
+
+// adicionar curva
+newCurvaBtn.addEventListener("click", function(event){
+    if (selectedCurve == -1 || curves[selectedCurve].length > 1){
+        stateCanvas = 1;
+        var newCurve = [];
+        curves.push(newCurve);
+        selectedPoint.push(0);
+        selectedCurve++;
+        legendaText.innerText = "Clique no canvas para adicionar os pontos de controle.";
+    } else {
+        legendaText.innerText = "Finalize a curva atual antes de começar uma nova.";
+    }
+});
+
+delCurvaBtn.addEventListener("click", function(event){
+    if (curves.length > 0){
+        curves.splice(selectedCurve, 1);
+        selectedPoint.splice(selectedCurve, 1);
+        if (selectedCurve > 0){
+            selectedCurve--;
+        }
+        reDraw();
+    } else {
+        return;
+    }   
+});
+
+previousCurveBtn.addEventListener("click", function(event){
+    if (selectedCurve > 0){
+        selectedCurve--;
+        reDraw();
+    } else { 
+        return;
+    }
+});
+
+nextCurveBtn.addEventListener("click", function(event){
+    if (selectedCurve < curves.length - 2){
+        selectedCurve++;
+        reDraw();
+    } else { 
+        return;
+    }
+});
+
+// add pontos
+addPontoBtn.addEventListener("click", function(event){
+    stateCanvas = 1;
+    legendaText.innerText = "Clique no canvas para adicionar os pontos de controle.";
+});
+
+// deletar o ponto selecionado
+delPontoBtn.addEventListener("click", function(event){
+    if (curves[selectedCurve].length > 0){
+        curves[selectedCurve].splice(selectedPoint[selectedCurve], 1);
+        selectedPoint.splice(selectedCurve, 1);
+        if (curves[selectedCurve].length == 0){
+            // remove a curva, pois ela ficou vazia
+            curve.splice(selectedCurve, 1);
+            selectedPoint.splice(selectedCurve, 1);
+            if (selectedCurve > 0){
+                selectedCurve--;
+            }
+        }
+        reDraw();
+    } else {
+        return;
+    }
+});
+
+movePontoBtn.addEventListener("click", function(event){
+    stateCanvas = 2;
+    legendaText.innerText = "Arraste os pontos no canvas para reposicioná-los.";
+});
+
+previousPontoBtn.addEventListener("click", function(event){
+    if (selectedPoint[selectedCurve] > 0){
+        selectedPoint[selectedCurve]--;
+    } else { 
+        return;
+    }
+    reDraw();
+});
+
+nextPontoBtn.addEventListener("click", function(event){
+    if (selectedPoint[selectedCurve] < curves[selectedCurve].length - 1){
+        selectedPoint[selectedCurve]++;
+    } else { 
+        return;
+    }
+    reDraw();
+});
+
+// checkboxes
+
+// poligonos
+poligonosCheckbox.addEventListener("click", function(event){
+    showPoligonos = !showPoligonos;
+    reDraw();
+});
+
+// pontos
+pontosCheckbox.addEventListener("click", function(event){
+    showPoints = !showPoints;
+    reDraw();
+});
+
+// curvas
+curvasCheckbox.addEventListener("click", function(event){
+    showCurves = !showCurves;
+    reDraw();
+});
+
+// numero de avaliacoes input
+numAvaliacoesInput.addEventListener("keyup", function(event){
+    var input = event.target.value;
+    numAvaliacoes = parseInt(input);
+    reDraw();
+});
